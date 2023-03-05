2a1d1745bccb5c1fc71814d035650a036ba57f11
Michael S <mshyer@outlook.com>
2023-03-05 13:55:39 -0500

    1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -28,39 +28,29 @@ const Content = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ],
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  const parts = [part1, part2, part3];  
-  const total = parts.reduce((sum, partObj) => sum + partObj.exercises);
+  const total = course.parts.reduce((sum, partObj) => sum + partObj.exercises);
 
   return (
-    // <div>
-    //   <h1>{course}</h1>
-    //   <p>
-    //     {part1} {exercises1}
-    //   </p>
-    //   <p>
-    //     {part2} {exercises2}
-    //   </p>
-    //   <p>
-    //     {part3} {exercises3}
-    //   </p>
-    //   <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    // </div>
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
       <Total total={total}/>
     </div>
   )
